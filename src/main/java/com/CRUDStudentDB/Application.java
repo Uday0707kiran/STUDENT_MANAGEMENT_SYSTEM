badfe9ce55e60bf4678a6b37f7b186ed5d9fbe47
UDAYKIRAN <udaykiransl07@gmail.com>
2026-04-04 15:38:20 +0530

    disabled jpa

--- src/main/java/com/CRUDStudentDB/Application.java
+++ src/main/java/com/CRUDStudentDB/Application.java
@@ -1,10 +1,12 @@
-package com.CRUDStudentDB;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
