fa7901e8c19dd0614ccd6a9985d33f4a30e3f8b6
UDAYKIRAN <udaykiransl07@gmail.com>
2026-04-04 16:29:47 +0530

    fix bean issue

--- src/main/java/com/CRUDStudentDB/Application.java
+++ src/main/java/com/CRUDStudentDB/Application.java
@@ -2,9 +2,8 @@ package com.CRUDStudentDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/CRUDStudentDB/Repository/StudentRepositry.java
+++ src/main/java/com/CRUDStudentDB/Repository/StudentRepositry.java
@@ -1,10 +1,11 @@
 package com.CRUDStudentDB.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 import com.CRUDStudentDB.Entity.Student;
 
+@Repository
 public interface StudentRepositry extends JpaRepository<Student,String> 
 {
 
